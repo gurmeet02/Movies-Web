ae3e9a5a68de1ab1cf3aec852e8e96f464258b33
gurmeet02 <135627176+gurmeet02@users.noreply.github.com>
2024-03-21 21:04:46 +0500

    React Router Setup, TailwindCSS Added

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">Hello</h1>
+    </section>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Trending/Trending.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Trending = () => {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">hash</h1>
+    </section>
+  );
+};
+
+export default Trending;
